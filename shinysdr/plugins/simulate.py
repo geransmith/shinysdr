814334fcb488d4ec3c55bf5d097e137bbe3454b7
Geran <8031619+geransmith@users.noreply.github.com>
2026-03-05 22:01:32 -0800

    Refactor rational resampler import for compatibility

--- shinysdr/plugins/simulate.py
+++ shinysdr/plugins/simulate.py
@@ -25,13 +25,7 @@ from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import channels
 from gnuradio import gr
-try:
-    from gnuradio.filter import rational_resampler_ccf, rational_resampler_fff
-except ImportError:
-    # Fallback for older GNU Radio versions
-    from gnuradio import filter as grfilter
-    rational_resampler_ccf = grfilter.rational_resampler_ccf
-    rational_resampler_fff = grfilter.rational_resampler_fff
+from gnuradio.filter import rational_resampler
 
 
 from shinysdr.devices import Device, IRXDriver
@@ -236,7 +230,7 @@ class _SimulatedTransmitter(gr.hier_block2, ExportedState):
         else:
             raise Exception('don\'t know how to supply input of type %s' % modulator_input_type)
         
-        rf_resampler = rational_resampler_ccf(
+        rf_resampler = rational_resampler.rational_resampler_ccf(
             interpolation=int(rf_rate),
             decimation=int(modulator.get_output_type().get_sample_rate()))
         self.__rotator = blocks.rotator_cc(rotator_inc(rate=rf_rate, shift=freq))
